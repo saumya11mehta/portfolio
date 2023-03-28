575f927d67702fb755f342d2844cc6c7d9d1de69
saumya11mehta <115432888+saumya11mehta@users.noreply.github.com>
2023-03-28 13:24:55 -0400

    fix : fixed hero description text spacing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -42,7 +42,7 @@ const Hero = () => {
             {"Hi, I'm "}<span className="text-violet-600">Saumya</span>
           </h1>
           <p className={`font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2 text-white-100`}>
-            {"I develop dynamic web applications"}<br className="sm:block hidden" />
+            {"I develop dynamic web applications "}<br className="sm:block hidden" />
             {"and interactive user interfaces"}
           </p>
         </div>
