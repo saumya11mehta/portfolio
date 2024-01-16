350735f535c0d19be78e64af658be2f117c78607
saumya11mehta <115432888+saumya11mehta@users.noreply.github.com>
2024-01-16 10:18:26 -0500

    fix: Fixed ThemeSwithcer Layout for mobile devices

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -67,16 +67,17 @@ const Navbar = () => {
           <Image
             src={toggle ? close : menu}
             alt="menu"
-            className="w-[28px] h-[28px] object-contain"
+            className="w-[28px] h-[28px] object-contain mr-5"
             onClick={() => setToggle(!toggle)}
           />
+          
+          <ThemeSwitcher/>
 
           <div
             className={`${
               !toggle ? "hidden" : "flex"
             } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
-            <ThemeSwitcher/>
             <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
               {navLinks.map((nav) => (
                 <li
