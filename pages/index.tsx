2fc098e907ed0d23306830443f22c8949b3d6364
saumya11mehta <115432888+saumya11mehta@users.noreply.github.com>
2024-01-16 13:41:30 -0500

    build : Added manifest

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
       <Head>
         <title>Saumya Mehta | Full Stack Developer</title>
         <link rel="icon" href={favicon.src} />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#3b82f6" />
       </Head>
       <div className="relative z-0 bg-gradient-to-tl from-blue-500 to-primary dark:bg-gradient-to-bl dark:from-primary dark:to-black">
